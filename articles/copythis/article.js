4980a21025cd3c08094e41b33c28d2809736d08a
The Eagles <141795010+the-eagle-nest@users.noreply.github.com>
2023-11-27 19:15:03 -0800

    Update 5b51

---
+++ articles/copythis/article.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Values to be customized
+  var articleTitle = "Replace this with the article title";
+  var articleContent = "Replace this with your article";
+  var articleWriter = "Replace this with the writer's name";
+  var articleDescription = "Replace this with a short description of the article";
+
+// DO NOT EDIT BELOW HERE.  IT WILL BREAK THE WEBSITE.
+
+  var articleTitleElement = document.getElementById('article-title');
+  articleTitleElement.innerText = articleTitle;
+  document.getElementById('article-content').innerHTML = articleContent;
+  var articleWriterElement = document.getElementById('article-writer');
+  articleWriterElement.innerText = "Written by: " + articleWriter;
+  var articleDescriptionElement = document.getElementById('article-description');
+    articleDescriptionElement.innerText = articleDescription;
+});
+
+function openPage(pageName) {
+  switch (pageName) {
+    case 'lunchMenu':
+      window.location.href = 'lunch_menu.html';
+      break;
+    case 'liveStream':
+      window.location.href = 'live_stream.html';
+      break;
+    case 'artCompetition':
+      window.location.href = 'art_competition.html';
+      break;
+    case 'vote':
+      window.location.href = 'vote.html';
+      break;
+    case 'digitalNewspaper':
+      window.location.href = 'digital_newspaper.html';
+      break;
+    default:
+      console.log('Invalid page name:', pageName);
+  }
+}
